b4c1cd5bdf3abc04e9b3fc8094fc0ba4d859e71f
Xianghbb <140491659+Xianghbb@users.noreply.github.com>
2025-06-20 08:04:16 +0000

    [automatic commit] 2025-06-20 08:04:16 UTC 122.150.228.164

--- part1/app.js
+++ part1/app.js
@@ -56,7 +56,9 @@ app.get('/api/walkrequests/open', async (req, res) => {
             JOIN Users u ON d.owner_id=u.user_id
             WHERE wr.status='open'
             ORDER BY wr.requested_time
-            `)
+            `);
+
+            res
     }
 });
 
