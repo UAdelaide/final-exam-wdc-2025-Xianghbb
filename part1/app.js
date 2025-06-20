7a6e14af42b1f5b4e0a16a223e7eba1e150ba993
Xianghbb <140491659+Xianghbb@users.noreply.github.com>
2025-06-20 08:45:04 +0000

    [automatic commit] 2025-06-20 08:45:04 UTC ;; communications error to 208.67.222.222#53: timed out 122.150.228.64

--- part1/app.js
+++ part1/app.js
@@ -51,7 +51,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
     try {
         const [rows]=await db.execute(`
             SELECT wr.request_id, d.name as dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username as owner_username
-            FROM WalkRequest wr
+            FROM WalkRequests wr
             JOIN Dogs d ON wr.dog_id=d.dog_id
             JOIN Users u ON d.owner_id=u.user_id
             WHERE wr.status='open'
