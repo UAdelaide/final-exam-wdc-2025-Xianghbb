01af2f53994af8bf8da5bd4f6b52955bb43a9450
Xianghbb <140491659+Xianghbb@users.noreply.github.com>
2025-06-20 08:26:11 +0000

    [automatic commit] 2025-06-20 08:26:11 UTC 122.150.228.164

--- part1/app.js
+++ part1/app.js
@@ -73,6 +73,7 @@ app.get('/api/walkers/summary', async (req, res) => {
             u.username as walker_username,
             COUNT(wr.rating_id) as total_ratings,
             CASE
-            WHEN COUNT(WR.RATING_ID)>0 THEN ROUND(AVG(wr))`)
+            WHEN COUNT(WR.RATING_ID)>0 THEN ROUND(AVG(wr.rating), 1)
+            `)
     }
 });
