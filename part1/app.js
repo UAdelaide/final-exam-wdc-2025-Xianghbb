1c6bc77b50c2f8b817fed2dd2fa0be6a0d76916f
Xianghbb <140491659+Xianghbb@users.noreply.github.com>
2025-06-20 08:01:02 +0000

    [automatic commit] 2025-06-20 08:01:02 UTC 122.150.228.64

--- part1/app.js
+++ part1/app.js
@@ -50,7 +50,7 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
         const [rows]=await db.execute(`
-            S`)
+            SELECT`)
     }
 });
 
