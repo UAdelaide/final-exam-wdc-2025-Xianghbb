2d23ab29676ddc40703fa81d41faae379bc13659
Xianghbb <140491659+Xianghbb@users.noreply.github.com>
2025-06-20 05:51:34 +0000

    [automatic commit] 2025-06-20 05:51:34 UTC 122.150.228.73

--- part1/app.js
+++ part1/app.js
@@ -19,7 +19,8 @@ let db;
         db=await mysql.createConnection({
             host='localhost',
             user:'root',
-            
+            password:'',
+            database:'Dog'
         })
     }
 })
