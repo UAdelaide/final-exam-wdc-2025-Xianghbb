32349a6a57f1bba49a7e1b63c098336752453f3a
Xianghbb <140491659+Xianghbb@users.noreply.github.com>
2025-06-20 08:02:53 +0000

    [automatic commit] 2025-06-20 08:02:53 UTC 122.150.228.73

--- part1/app.js
+++ part1/app.js
@@ -52,7 +52,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
         const [rows]=await db.execute(`
             SELECT wr.request_id, d.name as dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username as owner_username
             FROM WalkRequest wr
-            JOIN Dogs d ON wr`)
+            JOIN Dogs d ON wr.dog_id=d.dog_id
+            JOIN Users u ON d.owner_id=`)
     }
 });
 
