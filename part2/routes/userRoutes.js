a1fa7cdbed6dbaad24a794f450d0fb864620b037
Xianghbb <140491659+Xianghbb@users.noreply.github.com>
2025-06-21 02:22:57 +0000

    [automatic commit] 2025-06-21 02:22:57 UTC 122.150.228.64

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -60,7 +60,7 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) {
-      return res.status
+      return res.status(500).json({ error:''})
     }
   })
 });
