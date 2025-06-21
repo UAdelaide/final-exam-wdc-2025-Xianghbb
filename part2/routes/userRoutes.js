9bb85ec8b37a8700ffcdd0cf7affa977d201f3c4
Xianghbb <140491659+Xianghbb@users.noreply.github.com>
2025-06-21 05:06:12 +0000

    [automatic commit] 2025-06-21 05:06:12 UTC 122.150.228.185

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -67,8 +67,10 @@ router.get('/me/dogs', async (req, res) => {
 
   try {
     const [rows]=await db.query(`
-      SELECT dog_id, name, size FROM Dogs WHERE owner_id
-      `)
+      SELECT dog_id, name, size FROM Dogs WHERE owner_id=?
+      `, [ownerId]);
+
+      res.json
   }
 })
 
