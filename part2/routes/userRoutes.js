3c8f748a77842e31909cffcd07e259fe6c0dd0ba
Xianghbb <140491659+Xianghbb@users.noreply.github.com>
2025-06-21 05:04:10 +0000

    [automatic commit] 2025-06-21 05:04:10 UTC ;; communications error to 208.67.222.222#53: timed out 122.150.228.64

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -61,7 +61,7 @@ router.post('/login', async (req, res) => {
 // get all dogs owned by the currently logged-in owner
 router.get('/me/dogs', async (req, res) => {
   if (!req.session.user || req.session.user.role !== 'owner') {
-    return res.status
+    return res.status(401).json({ error: ''})
   }
 })
 
